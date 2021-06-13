5a96a903c2fa445af33c247d55b430fc594239f0
cryswerton <cryswertons@gmail.com>
2021-06-13 00:55:22 -0300

    first commit

---
+++ frontend/src/components/template/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css'
+import React from 'react'
+
+export default props => 
+    <footer className="footer">
+        <span>
+            Desenvolvido por <strong>Cryswerton Silva</strong>
+        </span>
+    </footer>
